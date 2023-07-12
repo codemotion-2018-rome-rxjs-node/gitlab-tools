34320fa0d2d5b285d81ae816d6cc2784a21cd5f5
Enrico Piccinin <enrico.piccinin@gmail.com>
2023-07-12 19:01:03 +0200

    fetch all projects of group

---
+++ src/gitlab-functions/group-functions.ts
@@ -0,0 +1,42 @@
+import axios from "axios"
+import { from, map } from "rxjs"
+import { GroupCompact } from "./group.model"
+
+export function readGroup(gitLabUrl: string, token: string, groupId: string) {
+    const command = `https://${gitLabUrl}/api/v4/groups/${groupId}`
+    return from(axios.get(command, {
+        headers: {
+            "PRIVATE-TOKEN": token
+        }
+    })).pipe(
+        map(resp => {
+            return resp.data as GroupCompact
+        })
+    )
+}
+
+export function fetchGroupDescendantGroups(gitLabUrl: string, token: string, groupId: string) {
+    const command = `https://${gitLabUrl}/api/v4/groups/${groupId}/descendant_groups`
+    return from(axios.get(command, {
+        headers: {
+            "PRIVATE-TOKEN": token
+        }
+    })).pipe(
+        map(resp => {
+            return resp.data
+        })
+    )
+}
+
+export function fetchAllGroupProjects(gitLabUrl: string, token: string, groupId: string) {
+    const command = `https://${gitLabUrl}/api/v4/groups/${groupId}/projects?include_subgroups=true&per_page=100`
+    return from(axios.get(command, {
+        headers: {
+            "PRIVATE-TOKEN": token
+        }
+    })).pipe(
+        map(resp => {
+            return resp.data
+        })
+    )
+}
